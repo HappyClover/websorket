6571d5d162238a2f3f43b2b88193e80912fd79b7
shability-sw <swdevwing@gmail.com>
2021-05-17 16:38:10 +0900

    api키 테스트

--- router/api_module/api/router.js
+++ router/api_module/api/router.js
@@ -274,9 +274,9 @@ async function checkAPI(key, pool){
 
     const result = await pool.query(query,value);
 
-    console.log(result[0].id);
+    console.log(result[0].TextRow);
 
-    return result[0].id;
+    return result[0].TextRow;
 
     // var result = mysqlDB.query(query,value, function (err, rows, fields) {
     //     if (!err) {
