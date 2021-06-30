d396807d9222c690611f9b7d188d505381649ba5
shability-sw <swdevwing@gmail.com>
2021-06-30 11:17:23 +0900

    포트로그 정상화

--- router/control/detail.js
+++ router/control/detail.js
@@ -262,8 +262,8 @@ router.get('/control/charge/', async (req, res) => {
         "station.name, station.identifier, station.code as station_code, station_port.number " +
         "from station_usage_history " +
         "left join station_port on station_usage_history.port_id = station_port.id "+
-        "left join station on station_usage_history.station_id = station.id "
-        "left join admin on station_usage_history.user_id = admin.id "
+        "left join station on station_usage_history.station_id = station.id "+
+        "left join admin on station_usage_history.user_id = admin.id "+
         "left join user on station_usage_history.user_id = user.id ";
     //+""
     var value = [req.session.uid];
