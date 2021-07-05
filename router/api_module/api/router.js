0a67121a0c69a73b6a778e1ca2f917f3a384312e
shability-sw <swdevwing@gmail.com>
2021-07-05 18:10:59 +0900

    API 로그 처리

--- router/api_module/api/router.js
+++ router/api_module/api/router.js
@@ -356,4 +356,16 @@ function getTimeStamp() {
 
     return s;
 }
+
+function leadingZeros(n, digits) {
+    var zero = '';
+    n = n.toString();
+
+    if (n.length < digits) {
+        for (i = 0; i < digits - n.length; i++)
+            zero += '0';
+    }
+    return zero + n;
+}
+
 module.exports = router;
