b67614e6c563b8b68b713e02d95ce5a8ef434b4b
shability-sw <swdevwing@gmail.com>
2021-07-07 13:38:21 +0900

    충전로그 데이터 정리

--- router/control/detail.js
+++ router/control/detail.js
@@ -252,7 +252,7 @@ router.get('/control/charge/', async (req, res) => {
 
     //오늘일자
     var query = "select count(*) as cnt, date_format(date,'%m-%d') as time, " +
-        "if(start is null, 0, timediff(ifnull(charge_complete,end), start)) as cnt_time" +
+        "if(start is null, 0, timediff(ifnull(charge_complete,end), start)) as cnt_time " +
         "from station_usage_history " +
         "where date between date_format(date_add(curdate(), interval -2 day),'%y-%m-%d') and date_format(date_add(curdate(), interval 1 day), '%y-%m-%d') " +
         "group by date_format(date, '%m-%d')"
